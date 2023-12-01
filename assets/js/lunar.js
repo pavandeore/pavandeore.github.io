e0f3921b662b3a30b983ffe06171536cdf195dfa
pawan <pavandeore90@gmail.com>
2023-12-01 22:05:22 +0530

    previous portfolio delete

--- assets/js/lunar.js
+++
@@ -1,8 +0,0 @@
-$('.modal').on('show.bs.modal', function (e) {
-    if($(e.currentTarget).attr("data-popup")){
-        $("body").addClass("body-scrollable");
-    }
-});
-$('.modal').on('hidden.bs.modal', function (e) {
-    $("body").removeClass("body-scrollable");
-});
